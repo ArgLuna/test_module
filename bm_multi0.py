cb5fb8884fee43beb008a21dbeedcb1846d729df
ArgLuna <ian955246@gmail.com>
2019-07-10 04:04:36 +0800

    Update test case.

--- bm_multi0.py
+++ bm_multi0.py
@@ -4,20 +4,18 @@ from test import *
 import os, time
 
 def main():
-    modmode = 'nomod'
-    migmode = 'normal'
-    accpprob = ''
-
-    time.sleep(60)
     f = igunit.FileConf()
     f.load('file.conf')
+
+    time.sleep(60)
+    confname = 'multi_test0'
     conf = igunit.Conf()
-    conf.load('conf/multi_test0.conf')
+    conf.load('conf/' + confname + '.conf')
     conf.clearRaw()
     igunit.multitest(conf)
     data = analyze.Data(os.getpid())
     data.loadAll(conf.rawdir)
     data.summary()
-    data.save('data/multi_test0_' +f.getPostfix())
+    data.save('data/' + confname + '_' + f.getPostfix())
 
 main()
